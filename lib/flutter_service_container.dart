066f9e1e36846e58ba139386bbd539c9b288328d
codelover <codelovercc@gmail.com>
2025-09-02 20:06:56 +0800

    export service_container library

--- lib/flutter_service_container.dart
+++ lib/flutter_service_container.dart
@@ -1,6 +1,8 @@
 /// Provide useful widgets for service_container package.
 library;
 
+export 'package:service_container/service_container.dart';
+
 export 'src/logging.dart';
 export 'src/service_widgets.dart';
 export 'src/services.dart';
